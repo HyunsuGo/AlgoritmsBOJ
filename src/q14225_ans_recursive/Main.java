ca5619721e95a34483113d91d64c2bfa300f17e0
Hyunsu <ghs_0_@naver.com>
2020-02-23 22:49:52 +0900

    BF ans recursive

---
+++ src/q14225_ans_recursive/Main.java
@@ -0,0 +1,33 @@
+package q14225_ans_recursive;
+
+import java.util.Scanner;
+
+public class Main {
+	static boolean[] c = new boolean[20*100000+10];
+	static int[] a = new int [20];
+	static int n;
+	
+	static void go(int i, int sum) {
+		if(i==n) {
+			c[sum]=true;
+			return;
+		}
+		go(i+1, sum+a[i]);
+		go(i+1, sum);
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		for(int i=0; i<n; i++) {
+			a[i]=sc.nextInt();
+		}
+		go(0, 0);
+		for(int i=1;; i++) {
+			if(c[i]==false) {
+				System.out.println(i);
+				break;
+			}
+		}
+	}
+}
